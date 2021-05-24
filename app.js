db083b1cd614fc2018270aba9417e10da192bb6c
Greg Davis <djgreg@djgrego.com>
2021-05-23 22:11:54 -0500

    added live current date, time, logName, and AirDate

---
+++ app.js
@@ -0,0 +1,20 @@
+function time() {
+    let date = new Date();
+    let time = date.toLocaleTimeString();
+    let options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' };
+    let day = date.toLocaleDateString('en-US');
+
+    let d = new Date();
+    let mm = d.getMonth() + 1;
+    let dd = d.getDate();
+    let yy = d.getFullYear();
+
+    document.getElementById("currentTime").innerHTML = time;
+    document.getElementById('currentDay').innerHTML = day;
+    document.getElementById('logName').innerHTML = `Log Name: KBIU-FM-DA-${mm}-${dd}-${yy}`;
+    document.getElementById('airDate').innerHTML = `Air Date: ${date.toLocaleDateString('en-US')}`;
+}
+
+setInterval(function() {
+    time();
+},1000);
