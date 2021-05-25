280aae85271b267681b07f1d3620cd49dff74b1e
Greg Davis <djgreg@djgrego.com>
2021-05-24 23:11:18 -0500

    added vtButton change on press

--- app.js
+++ app.js
@@ -18,3 +18,31 @@ function time() {
 setInterval(function() {
     time();
 },1000);
+
+
+// Let's try a record button
+
+let vtButtonStatus = "vt";
+let vtButton = document.getElementById("vtButton");
+vtButton.addEventListener("click", function(){
+    if (vtButtonStatus === "vt") {
+        vtButtonStatus = "record";
+        vtButton.innerHTML = "RECORD";
+        vtButton.style.backgroundImage = 'linear-gradient(rgb(255, 0, 0), rgb(210, 0, 0))';
+    } else if (vtButtonStatus === "record") {
+        vtButtonStatus = "play2";
+        vtButton.innerHTML = "PLAY<br>CUT 2";
+        vtButton.style.backgroundImage = 'linear-gradient(#43ff43, #018501)';
+    } else {
+        vtButtonStatus = "vt";
+        vtButton.innerHTML = "VOICE<br/>TRACK";
+    }
+ });
+
+// Stop button
+let stopButton = document.getElementById("stopButton");
+stopButton.addEventListener("click", function(){
+    vtButtonStatus = "vt";
+    vtButton.innerHTML = "VOICE<br/>TRACK";
+    vtButton.style.background = 'linear-gradient(#43ff43, #018501)';
+});
